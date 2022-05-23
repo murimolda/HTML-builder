70af2676c227e728d29069d8b92567e72d5afb3d
murimolda <35343729+murimolda@users.noreply.github.com>
2022-05-23 11:31:58 +0300

    feat: add 05-merse-styles solution

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,3 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 
+const bundleStyles = path.join(__dirname, 'project-dist', 'bundle.css');
+const originStyles = path.join(__dirname, 'styles');
+
+async function createBundle() {
+  try {
+    await fs.promises.writeFile(bundleStyles, '');  
+    let files = await fs.promises.readdir(originStyles, {withFileTypes: true });
+    for (let item of files) {
+      let fileType = path.parse(path.join(originStyles, item.name));
+      if (item.isFile() && fileType.ext === '.css') {
+        let data = '';
+        let readStream = fs.createReadStream(path.join(originStyles, item.name), 'utf-8');
+        readStream.on('data', (chunk) => (data += chunk));
+        readStream.on('end', () =>
+          fs.promises.appendFile(bundleStyles, data)
+        );
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+createBundle();
