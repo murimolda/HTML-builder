914f3dd736a88e6db37da3eecc41a62aeba4ab1b
murimolda <35343729+murimolda@users.noreply.github.com>
2022-05-23 10:47:51 +0300

    fix: change 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,73 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-const fsPromises = require('fs/promises');
 
-const originFolder = path.resolve(path.dirname(__filename), './files');
-const copyFolder = path.resolve(path.dirname(__filename), './files-copy');
+const originFolder = path.join(__dirname, 'files');
+const copyFolder = path.join(__dirname, 'files-copy');
 
-fs.stat(copyFolder, function(error) {
-  if (!error) {
-    deleteFiles(copyFolder);
-    copyFiles(originFolder, copyFolder);
-  }
-  else if (error.code === 'ENOENT') {
-    fs.mkdir(copyFolder, error => {
-      if(error) throw error;
-      copyFiles(originFolder, copyFolder);
-    });
-  }
-});
-
-const copyFiles = (folder, copy) =>{
-  fs.readdir(folder, (error, files) => {
-    if(error) throw error;
-    for (let i = 0; i < files.length; i++) {
-      fs.stat(path.resolve(folder, files[i]), (error, stats) => {
-        if(error){
-          throw error;
-        }
-        if (stats.isFile()) {
-          fs.copyFile(path.normalize(path.resolve(folder, files[i])), path.normalize(path.resolve(copy, files[i])), error => {
-            if(error) throw error;
-          });
-        }else if(stats.isDirectory()){
-          fs.mkdir(path.resolve(copy, files[i]), error => {
-            if(error) throw error;
-            copyFiles(path.resolve(folder, files[i]), path.resolve(copy, files[i]));
-          });
-        }
-      });
+async function copyFiles(folder, copy) {
+  await fs.promises.mkdir(copy, { recursive: true });
+  let files = await fs.promises.readdir(folder, { withFileTypes: true });
+  for (let item of files) {
+    if (item.isDirectory()) {
+      await copyFiles(path.join(folder, item.name), path.join(copy, item.name));
+    } else {
+      await fs.promises.copyFile(path.join(folder, item.name), path.join(copy, item.name));
     }
-  });
-};
-
-const deleteFiles = (folder) =>{
-  fs.readdir(folder, (error, files) => {
-    if(error) throw error;
-    for (let i = 0; i < files.length; i++) {
-      fs.stat(path.resolve(folder, files[i]), (error, stats) => {
-        if(error){
-          throw error;
-        }
-        if (stats.isFile()) {
-          fs.unlink(path.resolve(folder, files[i]), error => {
-            if(error) throw error;
-          });
-        }else {
-          deleteFiles(path.resolve(folder, files[i]));
-          removeDirectory(path.resolve(folder, files[i]));
-        }
-      });
-    }
-  });
-};
-
-const removeDirectory = async (dirPath) => {
-  try {
-    await fsPromises.rm(dirPath, { recursive: true });
-  } catch (error) {
-    console.log(error);
   }
-};
+}
 
+function changeFolder(folder, copy) {
+  fs.rm(copy, { recursive: true, force: true }, () => copyFiles(folder, copy));
+}
 
+changeFolder(originFolder, copyFolder);
